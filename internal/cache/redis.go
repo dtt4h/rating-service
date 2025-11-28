b8619938aa1f4eb75913f7f3b4c5906869a2a5bf
Your Name <dtt4h322@gmail.com>
2025-11-28 07:07:16 +0700

    feat: implement base logic for redis

--- internal/cache/redis.go
+++ internal/cache/redis.go
@@ -1,1 +1,36 @@
 package cache
+
+import (
+	"context"
+
+	"github.com/redis/go-redis/v9"
+)
+
+var (
+	ctx = context.Background()
+	rdb *redis.Client
+)
+
+// Создает и возвращает Redis клиенты
+func NewRedisClient() *redis.Client {
+	rdb = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	return rdb
+}
+
+// Возвращает существующий клиент
+func GetRedisClient() *redis.Client {
+	if rdb == nil {
+		rdb = NewRedisClient()
+	}
+	return rdb
+}
+
+// Проверяет подключение к Redis
+func Ping() error {
+	client := GetRedisClient()
+	return client.Ping(ctx).Err()
+}
