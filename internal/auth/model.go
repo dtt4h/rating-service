7835360ebaef452dff11772c7c6dbc603ac44eeb
Your Name <dtt4h322@gmail.com>
2025-11-28 07:32:58 +0700

    feat(auth): init struct types

--- internal/auth/model.go
+++ internal/auth/model.go
@@ -1,1 +1,22 @@
 package auth
+
+type User struct {
+	ID    string `json:"id"`
+	Login string `json:"login"`
+	Hash  string `json:"-"`
+}
+
+type RegisterRequest struct {
+	Login    string `json:"login" binding:"required,min=3,max=30"`
+	Password string `json:"password" binding:"required,min=6"`
+}
+
+type LoginRequest struct {
+	Login    string `json:"login" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+type AuthResponse struct {
+	Token string `json:"token"`
+	User  User   `json:"user"`
+}
